b93be6ad93ad24f31419488db9db125653a98aef
Svch3mk <45947938+Svch3mk@users.noreply.github.com>
2019-04-13 21:43:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,14 +9,7 @@ const client5 = new Discord.Client();
 
 
 
-client.on('message', message => {
-   if(!message.author.id === '442032455239860224') return
-  let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.startsWith('-قول')) {
-   
- message.channel.send(args);  
-}
-});
+
 client2.on('message', message => {
    if(!message.author.id === '442032455239860224') return
   let args = message.content.split(' ').slice(1).join(' ');
@@ -50,7 +43,6 @@ if(message.content.startsWith('-قول5')) {
 }
 });
 
-client.login(process.env.TOKEN);
 client2.login(process.env.TOKEN2);
 client3.login(process.env.TOKEN3);
 client4.login(process.env.TOKEN4);
